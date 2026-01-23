3b241f2817d3650b5f73edaf565445bb42d47bd9
jbgamer4all-maker <jbgamer4all@gmail.com>
2026-01-22 18:40:58 -0800

    sdrgsrgr

---
+++ src/robot-config.cpp
@@ -0,0 +1,21 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen.
+brain  Brain;
+
+digital_out matchloader = digital_out(Brain.ThreeWirePort.A);
+digital_out arm = digital_out(Brain.ThreeWirePort.B);
+
+//The motor constructor takes motors as (port, ratio, reversed), so for example
+//motor LeftFront = motor(PORT1, ratio6_1, false);
+
+//Add your devices below, and don't forget to do the same in robot-config.h:
+
+
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
